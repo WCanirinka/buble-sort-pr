9b78d146b2199867361e612df0f17a4702ede1fa
Wilfried Canirinka <canirinkawilfried@gmail,com>
2019-11-20 00:11:52 +0200

    Corrected the errors

--- file.rb
+++ file.rb
@@ -1,13 +1,13 @@
 # first method
 
 def bubble_sort(arr)
-  return arr.sort!
+  arr.sort!
 end
 
 # second method
 
 def bubble_sort_by(arr)
-  return arr.sort! { |x, y|
-           x.length - y.length
-         }
+  arr.sort! do |x, y|
+    x.length - y.length
+  end
 end
