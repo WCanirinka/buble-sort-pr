88cc876d77542d9a974a337bf60c058f9194b3b0
Wilfried Canirinka <canirinkawilfried@gmail,com>
2019-11-20 19:07:19 +0200

    Changed the code per the TSE the project specification

--- file.rb
+++ file.rb
@@ -2,14 +2,36 @@
 
 # first method bubble sort
 
-def bubble_sort(arr)
-  arr.sort!
+def bubble_sort(array)
+  sorted = false
+  while !sorted
+    sorted = true
+    (array.length - 1).times do |a|
+      if array[a + 1] < array[a]
+        array[a], array[a + 1] = array[a + 1], array[a]
+        sorted = false
+      end
+    end
+  end
+  print array
 end
 
-# second method
+bubble_sort([4, 3, 78, 2, 0, 2])
 
-def bubble_sort_by(arr)
-  arr.sort! do |x, y|
-    x.length - y.length
+def bubble_sort_by(array)
+  sorted = false
+  while !sorted
+    sorted = true
+    (arr.length - 1).times do |a|
+      if yield(array[a], array[a + 1]) > 0
+        array[a], array[a + 1] = array[a + 1], array[a]
+        sorted = false
+      end
+    end
   end
+  print array
+end
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length - right.length
 end
